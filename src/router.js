01e08620569a7e63951aa46f621d69618321ca85
asulin <asulin@jobtop.ru>
2018-07-23 05:34:24 +0300

    Add page for first task

--- src/router.js
+++ src/router.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
+const First = () => import('@/views/First.vue')
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/first',
+      name: 'first',
+      component: First
     }
   ]
 })
---
+++ src/views/First.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>1</div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'first',
+  computed: mapState('calculator', [
+    'fractions', 'operators'
+  ]),
+  methods: mapActions('calculator', [
+    'addFraction', 'updateFraction'
+  ])
+}
+</script>
